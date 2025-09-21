fa54372e71d0235ed68ce8ff13cd36c192be74df
Pica <picarts95@gmail.com>
2025-09-21 21:59:51 +0700

    Update vite.config.js

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from '3336feaa-b1b6-407d-bf1d-55381e57c0a6-00-fe3600a8bwji.pike.replit.dev'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+})'server.allowedhosts'3336feaa-b1b6-407d-bf1d-55381e57c0a6-00-fe3600a8bwji.pike.replit.dev 
+
